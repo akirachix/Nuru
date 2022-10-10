311983f6fab2a5cf0a77afb8887a64cbc547487c
wangare <marywangareee@gmail.com>
2022-10-10 09:00:59 +0300

    login

--- nuruCare/urls.py
+++ nuruCare/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("admin/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), 
+    
  
 ]
